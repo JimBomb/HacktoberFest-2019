b0a4133d423273d7e6311aa4d2aeb8dfa851faf1
JimBomb <44555752+JimBomb@users.noreply.github.com>
2019-10-23 01:38:40 +0530

    Complex Number

---
+++ Complex.java
@@ -0,0 +1,61 @@
+import java.util.*;
+class CreateComplex{
+    private double r;
+    private double c;
+    Scanner s = new Scanner(System.in);
+    void realInput(){
+        System.out.println("Enter value:");
+        r = s.nextDouble();
+        
+    }
+    void complexInput(){
+        System.out.println("Enter value:");
+        c = s.nextDouble();
+        
+        
+        
+    }
+    void displayComplex(){
+        System.out.println(r + "+ i"+c);
+    }
+    
+    CreateComplex add(CreateComplex c2){
+        CreateComplex c3 = new CreateComplex();
+        c3.r = r+ c2.r;
+        c3.c = c + c2.c;
+        return c3;
+        
+    }
+    CreateComplex subtract(CreateComplex c2){
+        CreateComplex c3 = new CreateComplex();
+        c3.r = r - c2.r;
+        c3.c = c - c2.c;
+        return c3;
+    }
+    
+}
+
+
+public class Complex {
+
+    
+
+    public static void main(String[] args) {
+        CreateComplex c1 = new CreateComplex();
+        CreateComplex c2 = new CreateComplex();
+        c1.complexInput();
+        c1.realInput();
+        c2.complexInput();
+        c2.realInput();
+        c1.displayComplex();
+        c2.displayComplex();
+        CreateComplex res = new CreateComplex();
+        CreateComplex res2 = new CreateComplex();
+        res = c1.add(c2);
+        res.displayComplex();
+        res2 = c1.subtract(c2);
+        res2.displayComplex();
+        
+    }
+    
+}
